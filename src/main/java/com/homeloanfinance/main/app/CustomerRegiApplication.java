5f81e9ff44d30cfb49600ffef8baa8b8ca02daf4
Mrunal <mrunalnyaykhor@gmail.com>
2023-06-07 09:40:15 +0530

    CustomerRegistration class changes

--- src/main/java/com/homeloanfinance/main/app/CustomerRegiApplication.java
+++ src/main/java/com/homeloanfinance/main/app/CustomerRegiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomerRegiApplication {
+      System.out.println("This is CustomerRegiApplication class");
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerRegiApplication.class, args);
